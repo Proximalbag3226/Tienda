680b6fd05445c99d1f6909c0374e3a7b881468e4
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-03-12 23:17:16 -0600

    Pequeñas modificaciones

--- src/Componentes/settitles.jsx
+++ src/Componentes/settitles.jsx
@@ -24,13 +24,32 @@ function PopularKeywordsComponent(props) {
                   <a className="gl-filter _keyword_5asl0_1">{props.producto4}</a>
                 </div>
                 <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
-                  <a className="gl-filter _keyword_5asl0_1">{props.producto5}</a>
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto5}</a>   
                 </div>
                 <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
                   <a className="gl-filter _keyword_5asl0_1">{props.producto6}</a>
                 </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto7}</a>
+                </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto8}</a>
+                </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto9}</a>
+                </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto10}</a>
+                </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto11}</a>
+                </div>
+                <div className="col--mb-s-6 col--tb-l-4 _keyword-wrapper_1t7ue_54">
+                  <a className="gl-filter _keyword_5asl0_1">{props.producto12}</a>
+                </div>
               </div>
             </div>
+            <img className = "david_laid"src={require('../images/david-laid.jpg')}/>
           </div>
         </div>
       </section>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -21,13 +21,29 @@ function Home(props){
                 <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"</h1>
             </figure>
             </div>
+            <div className="productgalery">
+            <section className="galeria">
+                <img src = {require("../images/producto1.jpg")}/>                
+                <img src = {require("../images/producto2.jpeg")}/>
+                <img src = {require("../images/producto3.jpeg")}/>
+                <img src = {require("../images/producto4.jpg")}/>
+                <img src = {require("../images/producto5.jpg")}/>
+                <img src = {require("../images/producto6.jpg")}/>
+            </section>
             <PopularKeywordsComponent
             producto1 = 'Tenis'
             producto2 = 'Suadaderas y hoddies'
             producto3 = 'Gorras'
             producto4 = 'Pants'
             producto5 = 'Playeras'
-            producto6 = 'Accesorios'/>
+            producto6 = 'Accesorios'
+            producto7 = 'Gym'
+            producto8 = 'Colaboraciones'
+            producto9 = 'Running'
+            producto10 = 'Skate'
+            producto11 = 'Running'
+            producto12 = 'Exclusivos'/>
+            </div>
         </div>
     )
 }
